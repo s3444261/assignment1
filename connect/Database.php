f688442470805f4b097464f233edd3cccd585ea1
Ubuntu <ubuntu@ip-172-31-12-220.ap-southeast-2.compute.internal>
2015-06-19 01:25:50 +0000

    added Database.php

---
+++ connect/Database.php
@@ -0,0 +1,25 @@
+<?php
+
+include '../db.php';
+
+class Database extends PDO {
+	private static $instance;
+
+	public static function getInstance() {
+		if (!isset(self::$instance)) {
+
+			$database= array(
+					'db_host' => DB_HOST,
+					'db_user' => DB_USER,
+					'db_pass' => DB_PW,
+					'db_name' => DB_NAME);
+
+			self::$instance = new Database('mysql:host='.$database['db_host'].';dbname='.$database['db_name'], $database['db_user'],$database['db_pass']);
+			
+			//comment out this line to disable PDO error messages
+			self::$instance->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING ); 
+		}
+		return self::$instance;
+	}
+}
+?>
