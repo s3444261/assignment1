4880b54781f1741eefca655d7b16bfe0589e61f7
Ubuntu <ubuntu@ip-172-31-12-220.ap-southeast-2.compute.internal>
2015-06-19 01:52:40 +0000

    added index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+include 'view/config.php';
+include 'connect/Database.php';
+include 'view/header.php';
+?>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Assignment 1</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Search</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-6 col-md-offset-3">
+<?php 
+include 'view/search.php';
+?> 
+    </div>
+  </div>
+</div>
+<?php 
+include 'view/footer.php';
+?>
