7b0f3fdeb793a15f6244d28f01ff48138ad28e4c
Ubuntu <ubuntu@ip-172-31-12-220.ap-southeast-2.compute.internal>
2015-06-19 01:54:42 +0000

    added view related files

---
+++ view/search.php
@@ -0,0 +1,35 @@
+<?php
+?>
+<form>
+  <div class="form-group">
+    <label for="wineName">Wine Name or Part Thereof</label>
+    <input type="text" class="form-control" id="wineName" placeholder="Wine Name">
+  </div>
+  <div class="form-group">
+    <label for="wineryName">Winery Name or Part Thereof</label>
+    <input type="text" class="form-control" id="wineryName" placeholder="Winery Name">
+  </div>
+  <div class="form-group">
+    <label for="wineRegion">Wine Region</label>
+    <select class="form-control">
+	  <option>1</option>
+	  <option>2</option>
+	  <option>3</option>
+	  <option>4</option>
+	  <option>5</option>
+	</select>
+  </div>
+  <div class="form-group">
+    <label for="grapeVariety">Grape Variety</label>
+    <select class="form-control">
+	  <option>1</option>
+	  <option>2</option>
+	  <option>3</option>
+	  <option>4</option>
+	  <option>5</option>
+	</select>
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
+<?php
+?>
